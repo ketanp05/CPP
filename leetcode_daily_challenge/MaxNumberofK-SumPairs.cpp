9e215b6a37bc6812870a23a7f890fc9fc50c61c8
Ketansingh Patil <patilketan125@gmail.com>
2022-05-04 23:33:54 +0530

    Add files via upload

---
+++ leetcode_daily_challenge/MaxNumberofK-SumPairs.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// using namespace std;
+
+class Solution{
+public:
+    int Compute(std::vector<int> v, int key){
+        // return pairs whose sum equals key
+        // int paircount = 0;
+        
+        // for(int i = 0; i < v.size() ; i++){
+        //     for(int j = i + 1; j < v.size(); j++){
+        //         if(v[i] + v[j] == key){
+        //             std::cout << v[i] << v[j] << std::endl;
+        //             paircount++;
+        //             std::cout << "pair count: " << paircount << std::endl;
+        //         }
+        //     }
+        // }
+        
+        // return paircount;
+        
+        std::sort(nums.begin(), nums.end());
+        
+        
+        int i = 0; int j = nums.size() - 1;
+        int count = 0; int sum = 0;
+        
+        while(i < j){
+            sum = nums[i] + nums[j];
+            if(sum == key){
+                count++;
+                i++;
+                j--;
+            }else if(sum > key){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        
+        return count;
+    }
+};
+
+int main() {
+	// your code goes here
+	Solution s;
+	
+	std::vector<int> nums{3,1,3,4,3};
+	int key = 6;
+	
+	std::cout << s.Compute(nums, key) << std::endl;
+	
+	return 0;
+}
