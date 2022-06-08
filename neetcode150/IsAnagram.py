db7fd445db18b1e54d33ef39f46ea616ef1e47a1
Ketansingh Patil <patilketan125@gmail.com>
2022-06-08 17:17:38 +0530

    Add files via upload

---
+++ neetcode150/IsAnagram.py
@@ -0,0 +1,46 @@
+class Solution:
+    def isAnagram(self, s:str, t:str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        CountS, CountT = {}, {}
+        
+        for letter_s in s:
+            CountS[letter_s] = 1 + CountS.get(letter_s, 0)
+        
+        for letter_t in t:
+            CountT[letter_t] = 1 + CountT.get(letter_t, 0)
+            
+        for key in CountS:
+            if CountS[key] == CountT[key]:
+                return True
+        return False
+        
+        # if(len(s) != len(t)):
+        #     return False
+        # count = 0
+        # for i in range(len(s)):
+        #     if t[i] in s:
+        #         count += 1
+    
+        # if count == len(s):            
+        #     return True
+    
+    # def isAnagram(self, s:str, t:str) -> bool:
+    #     return sorted(s) == sorted(t)
+    
+    # def isAnagram(self, s:str, t:str) -> bool:
+    #     return Counter(s) == Counter(t)
+        
+
+def main():
+    s = "aacc"
+    t = "ccac"
+    obj = Solution()
+    print(obj.isAnagram(s, t))
+    
+if __name__ == "__main__":
+    main()
+
+# optimal
+
