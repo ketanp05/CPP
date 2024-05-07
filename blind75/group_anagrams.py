56cdbce209d3deedc0fa58b2db781c6c353364b5
Ketansingh Patil <patilketan125@gmail.com>
2024-05-07 00:06:16 -0400

    complete solution to the group anagrams problem

--- blind75/group_anagrams.py
+++ blind75/group_anagrams.py
@@ -1,8 +1,23 @@
 from typing import List
+from collections import defaultdict
 
 class GroupAnagramsProblem:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        # if there are no anagram groups then return a dict with list
+        # now you cannot have List as your key so later you will see a conversion from a list to a tuple
+        result_map = defaultdict(list)
+
+        # i will increment the count at the letters relevant index position to 1
+        # for eg: e will have the count 1 at index 4 when the input string has e
+        for string_in_strs in strs:
+            count = [0] * 26 # 26 letters of an alphabet
+
+            for character in string_in_strs:
+                count[ord(character) - ord('a')] += 1
+
+            result_map[tuple(count)].append(string_in_strs)
+        
+        return result_map.values()
 
 def main():
     gap = GroupAnagramsProblem()
