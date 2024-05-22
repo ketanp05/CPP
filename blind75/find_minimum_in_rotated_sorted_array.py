81d3c35937c274cb120eaa8db81f402da49b37a1
Ketansingh Patil <patilketan125@gmail.com>
2024-05-22 13:31:15 -0400

    some minor changes to the find_min_in_rot_sorted_array problem. no major changes. correct and ready to by committed to prod. no need to review

--- blind75/find_minimum_in_rotated_sorted_array.py
+++ blind75/find_minimum_in_rotated_sorted_array.py
@@ -3,9 +3,9 @@ from typing import List
 class FindMinimum:
     def find_minimum_rotated_sorted_array(self, nums: List[int]) -> int:
         l, r = 0, len(nums)-1
-        result = nums[l]
+        result = nums[0]
 
-        while l < r:
+        while l <= r:
             # if array is already sorted then the min will be the first element
             if nums[l] < nums[r]:
                 result = min(nums[l], result)
@@ -14,7 +14,7 @@ class FindMinimum:
             mid = (l+r)//2
             result = min(result, nums[mid])
 
-            if nums[mid] > nums[l]:
+            if nums[mid] >= nums[l]:
                 l = mid+1
             else:
                 r = mid-1
@@ -26,7 +26,8 @@ class FindMinimum:
 def main():
     fm = FindMinimum()
     # input: [4,5,6,7,0,1,2]
-    print(fm.find_minimum_rotated_sorted_array([4,5,6,7,0,1,2]))
+    # input: [2,1]
+    print(fm.find_minimum_rotated_sorted_array([2,1]))
 
 if __name__ == "__main__":
     main()
