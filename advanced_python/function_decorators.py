0105e0572199bec273c16312666f3ef3bbb5c171
Ketansingh Patil <patilketan125@gmail.com>
2024-06-04 18:05:54 -0400

    example of how funciton decorators work

---
+++ advanced_python/function_decorators.py
@@ -0,0 +1,11 @@
+# # og memoize function
+# def memoize(fn):
+#     cache = {}
+#     def memoize_the_fn_function():
+
+# # the function thats gonna get memoized
+# @memoize
+# def fibonaci_memo(n):
+#     pass
+
+# print(fibonaci_memo(10))
